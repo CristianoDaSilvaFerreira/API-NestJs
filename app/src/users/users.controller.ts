6435f91d748a4eaaf17b67b642ecbffd52ba9d37
Cristiano <cristianodevsystem@gmail.com>
2022-02-04 20:38:17 -0300

    :rocket: Rota da função de criar usuários

--- app/src/users/users.controller.ts
+++ app/src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from './interface/user.interface';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    // Constructor
+    constructor(private readonly userService: UsersService) {}
+
+    // Rota de adiconar usuários
+    @Post()
+    create(@Body() user: User): User {
+        return this.userService.create(user);
+    }
+}
--- app/src/users/users.service.ts
+++ app/src/users/users.service.ts
@@ -1,4 +1,15 @@
+import { User } from './interface/user.interface';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    // Arrya para ser populado
+    private users: User[] = [];
+
+    // Função do create()
+    create(user: User): User {
+        this.users.push(user);
+        return user;
+    }
+}
