fdf80c36e58c2c37659019fb5d550b1506bbe74d
Cristiano <cristianodevsystem@gmail.com>
2022-02-05 10:01:43 -0300

    :rocket: Configurando o serviço do kafka

--- app/src/users/users.controller.ts
+++ app/src/users/users.controller.ts
@@ -1,12 +1,42 @@
 import { UserEntity } from './database/user.entity';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, OnModuleInit, Post } from '@nestjs/common';
 import { User } from './interface/user.interface';
 import { ApiBody } from '@nestjs/swagger';
+import { Client, ClientKafka, Transport } from '@nestjs/microservices';
 
 @Controller('users')
-export class UsersController {
+export class UsersController implements OnModuleInit {
+
+    // Configurando o Client de conexão com o Kafka
+    @Client({
+        transport: Transport.KAFKA,
+        options: {
+            client: {
+                clientId: 'user',
+                brokers: ['localhost:9092'],
+            },
+            consumer: {
+                groupId: 'user-consumer',
+                allowAutoTopicCreation: true
+            }
+        }
+    })
+
+    private client: ClientKafka;
+
+    async onModuleInit() {
+        const requestPatters = [
+            'find-all-user',
+            'create-user'
+        ];
+
+        requestPatters.forEach(async pattern => {
+            this.client.subscribeToResponseOf(pattern);
+            await this.client.connect();
+        })
+    }
 
     // Constructor
     constructor(private readonly userService: UsersService) {}
