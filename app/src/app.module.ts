060980d70f7b2b15f75349d7c48e7fac00a934ca
Cristiano <cristianodevsystem@gmail.com>
2022-02-04 23:41:17 -0300

    :rocket: Configurando o banco de dados Postgres + Docker

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,17 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    })
+    UsersModule,    
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService],
