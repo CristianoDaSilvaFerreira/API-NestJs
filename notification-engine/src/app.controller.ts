6290c4a710ee2293da55272575bdd04570036e7d
Cristiano <cristianodevsystem@gmail.com>
2022-02-07 21:54:28 -0300

    :rocket: Serviço de notificação

--- notification-engine/src/app.controller.ts
+++ notification-engine/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('notification-user')
+  sendEmail(@Payload() data: any): void {
+    console.log(data.value);
+  }
+
+  @MessagePattern('notification-user') 
+  sendPhone(@Payload() data: any): void {
+    console.log(data.value);
   }
 }
